7112527bf29d638e8d5586ffc17d351cafa45d56
mrbubble62 <martin@mrbubble.net>
2022-11-30 02:54:40 -0500

    Remove dead code and dual/quad mode  setting

--- LinuxCNC/Components/Upboard/upboardspi.c
+++ LinuxCNC/Components/Upboard/upboardspi.c
@@ -34,20 +34,15 @@
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
 static const char *device = "/dev/spidev2.0"; // use CS0 device, the CS1 is GPIO pin and slow (10x slower than RPi)
-static uint32_t mode;
-static uint8_t bits = 8;
+static uint32_t mode = 0;
+static uint8_t bits = 8; // bits per word
 static char *input_file;
 static char *output_file;
 static uint32_t speed = 5000000;
 static uint16_t delay = 0;
-static int verbose;
-static int transfer_size;
-static int iterations;
-static int interval = 5; /* interval in seconds for showing transfer rate */
 static int fd;
 
-static char *input_tx;
-
+static int verbose;
 static uint8_t debug = 0;
 
 static void pabort(const char *s)
@@ -127,21 +122,43 @@ int upboard_spi_close(void)
 	return 1;
 }
 
-// TBD
+// chip select polarity and phase
 void upboard_spi_setDataMode()
 {
-	//  BCM2835_SPI_MODE0 = 0,  /*!< CPOL = 0, CPHA = 0 */
 
-	mode |= SPI_CPHA;
+	int ret = 0;
+	if (fd < 0)
+		pabort("spi device is not open");
+
+	// CPHA= 0, CPOL = 0
+	// mode |= SPI_CPHA;
 	// mode |= SPI_CPOL;
+
+	/* WR is make a request to assign 'mode' */
+	int request = mode;
+	ret = ioctl(fd, SPI_IOC_WR_MODE32, &mode);
+	if (ret == -1)
+		pabort("can't set spi mode");
+	/* RD is read what mode the device actually is in */
+	ret = ioctl(fd, SPI_IOC_RD_MODE32, &mode);
+	if (ret == -1)
+		pabort("can't get spi mode");
+	/* Drivers can reject some mode bits without returning an error.
+	 * Read the current value to identify what mode it is in, and if it
+	 * differs from the requested mode, warn the user.
+	 */
+	if (request != mode)
+		printf("WARNING device does not support requested mode 0x%x\n",
+			   request);
 }
 
 // Set SPI speed Hz
-void upboard_spi_set_speed(uint32_t speed)
+void upboard_spi_set_speed(uint32_t _speed)
 {
-	int ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+	int ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &_speed);
 	if (ret == -1)
 		pabort("can't set max speed hz");
+	speed = _speed;
 }
 
 // internal spi transfer
@@ -156,23 +173,9 @@ void transfer(int fd, uint8_t const *tx, uint8_t const *rx, size_t len)
 		.delay_usecs = delay,
 		.speed_hz = speed,
 		.bits_per_word = bits,
+		.rx_nbits = 1,
+		.tx_nbits = 1,
 	};
-
-	if (mode & SPI_TX_QUAD)
-		tr.tx_nbits = 4;
-	else if (mode & SPI_TX_DUAL)
-		tr.tx_nbits = 2;
-	if (mode & SPI_RX_QUAD)
-		tr.rx_nbits = 4;
-	else if (mode & SPI_RX_DUAL)
-		tr.rx_nbits = 2;
-	if (!(mode & SPI_LOOP))
-	{
-		if (mode & (SPI_TX_QUAD | SPI_TX_DUAL))
-			tr.rx_buf = 0;
-		else if (mode & (SPI_RX_QUAD | SPI_RX_DUAL))
-			tr.tx_buf = 0;
-	}
 
 	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
 	if (ret < 1)
